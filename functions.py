54e441e727de680ce2c85129a1b220b129da5da7
lumivalsa <lumivalsa@gmail.com>
2022-09-21 14:03:52 +0200

    actualizacion

--- functions.py
+++ functions.py
@@ -305,12 +305,12 @@ def time_now():
 
     return diaSemana, dia, mes, anyo, hora, minuto, segundo
 
-def feature_visual(url):
+def feature_visual(csv):
 '''Función que permite importar el archivo csv y devolver un analisis de cada columna del dataframe.
 (Comparativa por columnas, mapa de calor, mapa de correlaciones.)'''    
-    df=pd.read_csv(url)
+    df=pd.read_csv(csv)
     profile=ProfileReport(df, title="Pandas Profiling Report")
-    return print(profile)
+    return (profile)
 
 def Feature_analisis(df):
     '''Análisis incial del df '''
